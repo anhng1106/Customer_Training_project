9057b8565eb58127dabb4fea0c1852200c3c9d7b
anhng1106 <ntvanh1106@gmail.com>
2024-04-08 03:05:52 +0300

    fetch customer's training in training list

--- src/components/Traininglist.jsx
+++ src/components/Traininglist.jsx
@@ -7,7 +7,6 @@ import dayjs from "dayjs";
 
 function Traininglist() {
   const [trainings, setTrainings] = useState([]);
-  const [customertraining, setCustomerTraining] = useState([]);
   const [colDefs, setColDefs] = useState([
     { field: "activity", filter: true },
     {
@@ -22,22 +21,48 @@ function Traininglist() {
 
   useEffect(() => {
     fetchTrainings();
-    fetchCustomerTrainings();
+    // fetchCustomerTrainings();
   }, []);
 
-  const fetchTrainings = () => {
-    getTrainings()
-      .then((data) => setTrainings(data._embedded.trainings))
-      .catch((err) => console.log(err));
+  const fetchTrainings = async () => {
+    // getTrainings()
+    //   .then((data) =>
+    //     setTrainings(
+    //       data._embedded.trainings.map((training) => ({
+    //         ...training,
+    //         customerName: training._links.customer.lastname,
+    //       }))
+    //     )
+    //   )
+    //   .catch((err) => console.log(err));
+
+    try {
+      const data = await getTrainings();
+      const trainingsWithCustomer = await Promise.all(
+        data._embedded.trainings.map(async (training) => {
+          // Assume getCustomerTraining fetches customer data from URL
+          const customerData = await fetch(training._links.customer.href).then(
+            (response) => response.json()
+          );
+          return {
+            ...training,
+            customerName: `${customerData.firstname} ${customerData.lastname}`,
+          };
+        })
+      );
+      setTrainings(trainingsWithCustomer);
+    } catch (err) {
+      console.log(err);
+    }
   };
 
-  const fetchCustomerTrainings = () => {
-    getCustomerTraining()
-      .then((data) =>
-        setCustomerTraining(data._embedded.trainings._links.customer)
-      )
-      .catch((err) => console.log(err));
-  };
+  // const fetchCustomerTrainings = () => {
+  //   getCustomerTraining()
+  //     .then((data) =>
+  //       setCustomerTraining(data._embedded.trainings._links.customer.lastname)
+  //     )
+  //     .catch((err) => console.log(err));
+  // };
 
   return (
     <>
